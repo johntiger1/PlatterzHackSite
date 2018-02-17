197dab85f9c8f9be535e77866a4904feb224e6a2
johntiger1 <john.person1@gmail.com>
2018-02-17 11:05:07 -0500

    refactor to be more generic; working

--- pizza_site/inner_pizza/urls.py
+++ pizza_site/inner_pizza/urls.py
@@ -3,11 +3,16 @@ from django.urls import path
 from . import views
 app_name = "inner_pizza"
 urlpatterns = [
-    path('index', views.index, name='index'),
-    path('more_pizza', views.another_req, name="more_piz"),
-    path('<int:topping_id>/', views.detail, name="topping_detail"),
+
+    path('', views.IndexView.as_view(), name= 'main'),
+    path('<int:pk>/', views.DetailView.as_view(), name='topping_detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
+    # path('index', views.index, name='index'),
+    # path('more_pizza', views.another_req, name="more_piz"),
+    # path('<int:topping_id>/', views.detail, name="topping_detail"),
     path('<int:topping_id>/vote/', views.vote, name='vote'),
-    path('<int:topping_id>/results/', views.results, name="results"),
-    path('', views.main_page, name="main"),
+    # path('<int:topping_id>/results/', views.results, name="results"),
+    # path('', views.main_page, name="main"),
     # path('my_endpoint', views.my_function)
 ]
--- pizza_site/inner_pizza/views.py
+++ pizza_site/inner_pizza/views.py
@@ -4,27 +4,55 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from .models import Topping
 # Create your views here.
-def index(request):
-    return HttpResponse("inner pizza ")
 
-def another_req(request):
-    return HttpResponse("more pizza stuff")
+class IndexView(generic.ListView):
+    template_name='inner_pizza/index.html'
+    context_object_name = 'list_of_toppings'
 
-def main_page(request):
-    # string_content = "the main page for inner_pizza. We currently have the following" \
-    #                  "toppings in the db\n\n"
-    # string_content+=str(Topping.objects.get(pk=1))
+    #this is if you want to set it once...
+    #queryset=Topping.objects.all()
 
-    list_of_toppings = Topping.objects.all()
-    template = loader.get_template("inner_pizza/index.html")
-    context = {
-    'list_of_toppings' : list_of_toppings,
+    # this is the new name
+    def get_queryset(self):
+        return Topping.objects.order_by('?')
 
-    }
-    return HttpResponse(template.render(context,request))
+    # this is the old legacy name
+    def get_query_set(self):
+        return Topping.objects.order_by('?')
+
+
+class DetailView(generic.DetailView):
+    model = Topping
+    template_name = 'inner_pizza/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Topping
+    template_name = 'inner_pizza/results.html'
+
+# def index(request):
+#     return HttpResponse("inner pizza ")
+#
+# def another_req(request):
+#     return HttpResponse("more pizza stuff")
+#
+
+
+# def main_page(request):
+#     # string_content = "the main page for inner_pizza. We currently have the following" \
+#     #                  "toppings in the db\n\n"
+#     # string_content+=str(Topping.objects.get(pk=1))
+#
+#     list_of_toppings = Topping.objects.all()
+#     template = loader.get_template("inner_pizza/index.html")
+#     context = {
+#     'list_of_toppings' : list_of_toppings,
+#
+#     }
+#     return HttpResponse(template.render(context,request))
 
 def vote(request, topping_id):
     topping = get_object_or_404(Topping, pk=topping_id)
@@ -39,19 +67,19 @@ def vote(request, topping_id):
 
     return HttpResponseRedirect(reverse('inner_pizza:results', args=(topping_id,)))
 
-
-def results(request, topping_id):
-    try:
-        topping_obj = Topping.objects.get(pk=topping_id)
-    except Topping.DoesNotExist:
-        raise Http404("Topping does not exist")
-    return render(request, 'inner_pizza/results.html', {'topping': topping_obj})
-
-
-def detail(request, topping_id):
-    try:
-        topping_obj = Topping.objects.get(pk=topping_id)
-    except Topping.DoesNotExist:
-        raise Http404("Topping does not exist")
-    return render(request, 'inner_pizza/detail.html', {'topping': topping_obj})
-    # return HttpResponse("you're looking at %s topping" % topping_id)
+#
+# def results(request, topping_id):
+#     try:
+#         topping_obj = Topping.objects.get(pk=topping_id)
+#     except Topping.DoesNotExist:
+#         raise Http404("Topping does not exist")
+#     return render(request, 'inner_pizza/results.html', {'topping': topping_obj})
+#
+#
+# def detail(request, topping_id):
+#     try:
+#         topping_obj = Topping.objects.get(pk=topping_id)
+#     except Topping.DoesNotExist:
+#         raise Http404("Topping does not exist")
+#     return render(request, 'inner_pizza/detail.html', {'topping': topping_obj})
+#     # return HttpResponse("you're looking at %s topping" % topping_id)
